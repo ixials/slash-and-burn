c864d2cc4673c3834a9ccfd23e1c1559af095777
Lillian Wang <galatheid@gmail.com>
2026-03-30 13:49:53 -0400

    add interactive content

--- script.js
+++ script.js
@@ -19,6 +19,8 @@ const poems = document.querySelectorAll(".poem");
 poems.forEach((poem) => {
   let blurTimeout;
   const text = poem.querySelector(".poem-text");
+  const content = poem.querySelectorAll(".interactive-content");
+
   text.addEventListener("mouseenter", () => {
     clearTimeout(blurTimeout);
     blurTimeout = setTimeout(() => {
@@ -31,4 +33,53 @@ poems.forEach((poem) => {
       text.classList.remove("revealed");
     }, 500);
   });
+
+  if (content) {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) {
+            content.forEach((item) => {
+              item.classList.remove("visible");
+            });
+          }
+        });
+      },
+      { threshold: 0 },
+    );
+    observer.observe(poem);
+  }
+});
+
+const music = document.getElementById("bg-music");
+
+document.querySelectorAll(".interactive-text").forEach((element) => {
+  element.addEventListener("click", () => {
+    music.play();
+  });
+});
+
+document.getElementById("jinzhi").addEventListener("click", function () {
+  const section = document.getElementById("jinzhi-content");
+  section.classList.toggle("visible");
+});
+
+document.getElementById("jia").addEventListener("click", function () {
+  const section = document.getElementById("jia-content");
+  section.classList.toggle("visible");
+});
+
+document.getElementById("mary").addEventListener("click", function () {
+  const section = document.getElementById("mary-content");
+  section.classList.toggle("visible");
+});
+
+document.getElementById("puerto-reyes").addEventListener("click", function () {
+  const section = document.getElementById("puerto-reyes-content");
+  section.classList.toggle("visible");
+});
+
+document.getElementById("iguazu").addEventListener("click", function () {
+  const section = document.getElementById("iguazu-content");
+  section.classList.toggle("visible");
 });
