9738468217123f8daa56276415c102125104358d
Lillian Wang <galatheid@gmail.com>
2026-04-11 03:10:17 -0400

    fix mobile layout

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// ======== MUSIC ========
+
+const music = document.getElementById("bg-music");
+
+document.querySelectorAll(".interactive-text").forEach((element) => {
+  element.addEventListener("click", () => {
+    music.play();
+  });
+});
+
+// ======== HORIZONTAL SCROLL ========
+
 horizontalContainer = document.querySelector(".container");
 
 horizontalContainer.addEventListener(
@@ -14,49 +26,52 @@ horizontalContainer.addEventListener(
   { passive: false },
 );
 
+// ======== POEMS ========
+
 const poems = document.querySelectorAll(".poem");
+const isHoverDevice = window.matchMedia("(hover: hover)").matches;
 
 poems.forEach((poem) => {
   let blurTimeout;
   const text = poem.querySelector(".poem-text");
   const content = poem.querySelectorAll(".interactive-content");
 
-  text.addEventListener("mouseenter", () => {
-    clearTimeout(blurTimeout);
-    blurTimeout = setTimeout(() => {
-      text.classList.add("revealed");
-    }, 500);
-  });
-  text.addEventListener("mouseleave", () => {
-    clearTimeout(blurTimeout);
-    blurTimeout = setTimeout(() => {
-      text.classList.remove("revealed");
-    }, 500);
-  });
+  if (isHoverDevice) {
+    text.addEventListener("mouseenter", () => {
+      clearTimeout(blurTimeout);
+      blurTimeout = setTimeout(() => {
+        text.classList.add("revealed");
+      }, 500);
+    });
 
-  if (content) {
+    text.addEventListener("mouseleave", () => {
+      clearTimeout(blurTimeout);
+      blurTimeout = setTimeout(() => {
+        text.classList.remove("revealed");
+      }, 500);
+    });
+  } else {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-          if (!entry.isIntersecting) {
+          if (entry.isIntersecting) {
+            text.classList.add("revealed");
+          } else {
+            text.classList.remove("revealed");
+
             content.forEach((item) => {
               item.classList.remove("visible");
             });
           }
         });
       },
-      { threshold: 0 },
+      {
+        threshold: 0.5,
+      },
     );
+
     observer.observe(poem);
   }
-});
-
-const music = document.getElementById("bg-music");
-
-document.querySelectorAll(".interactive-text").forEach((element) => {
-  element.addEventListener("click", () => {
-    music.play();
-  });
 });
 
 document.getElementById("jinzhi").addEventListener("click", function () {
@@ -91,11 +106,6 @@ document.getElementById("momentum").addEventListener("click", function () {
 
 document.getElementById("puerto-reyes").addEventListener("click", function () {
   const section = document.getElementById("puerto-reyes-content");
-  section.classList.toggle("visible");
-});
-
-document.getElementById("iguazu").addEventListener("click", function () {
-  const section = document.getElementById("iguazu-content");
   section.classList.toggle("visible");
 });
 
